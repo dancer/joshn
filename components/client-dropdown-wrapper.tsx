1bac8568eab7121f9cb126b067276a29a10bcfed
josh <josh@afterima.ge>
2025-09-22 02:33:49 +0100

    0

--- components/client-dropdown-wrapper.tsx
+++ components/client-dropdown-wrapper.tsx
@@ -15,7 +15,7 @@ export default function ClientDropdownWrapper() {
 	return (
 		<div className="absolute left-0 top-0 w-full flex justify-center pt-16 md:pt-20 pointer-events-none z-50">
 			<div className="w-full max-w-4xl px-4 md:px-16 relative">
-				<div className="absolute left-5 top-12 md:left-5 md:top-12 pointer-events-auto">
+				<div className="absolute right-4 top-8 md:left-5 md:top-12 pointer-events-auto">
 					<DropdownMenu />
 				</div>
 			</div>
